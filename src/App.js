26eeb658156c4eacec602bfae09d58a1e0e3930f
Skulljoker <skull27252@outlook.co.th>
2020-08-16 09:51:01 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import ABOUT from './components/ABOUT'
+import CONTACT from './components/CONTACT'
+import HOME from './components/HOME'
+function App() {
+  return (
+    <div className="App">
+      <HOME />
+      <ABOUT />
+      <CONTACT />
+    
+    </div>
+  );
+}
+
+export default App;
